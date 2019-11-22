99d793a380e1631764a771d0be4607eedabba0bd
Jon Gryn <jongryn@Jons-iMac.local>
2019-11-22 11:47:59 -0800

    Passing Callback Functions Around 11/22/19
    Introducing Maps
    
    Mapping Lists to Widgets
    
    final vs const
    
    Introducing “if” Statements

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -17,46 +18,57 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'How likely are you to recommend this app?', 
+      'answers': ['Highly Likely', 'More Likely', 'Likely', 'Unlikely', 'Highly Unlikely'],
+    },
+    {
+      'questionText': 'Why did you select that choice?',
+      'answers': ['Didn\'t like the app', 'The app was too hard to use', 'It didn\'t have the features I was looking for', 'The app was broken', 'It was a great app'],
+    },
+    {
+      'questionText': 'What was your most favorite part of the app?',
+      'answers': ['The look and feel', 'How user friendly it was', 'It had the features I was looking for', 'The filter calculator'],
+    }
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    // var aBool = true;
+    // aBool = false;
+
     setState(() {  
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('We have more questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
-    ];
+
+    // var dummy = const ['Hello'];
+    // dummy.add('Max');
+    // print(dummy);
+    // dummy = [];
+    // questions = []; // does not work if questions is a const
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('PolarPro'),
         ),
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]['questionText'],
             ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('Answer 2 chosen!'),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                // ...
-                print('Answer 3 chosen');
-              },
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
